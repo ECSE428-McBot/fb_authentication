3f532933c4b8c62cd754d9504612f141f4a87fdd
Nick Purdie <nick.purdie@mail.mcgill.ca>
2017-02-05 21:27:09 -0500

    a

--- index.php
+++ index.php
@@ -6,3 +6,4 @@ $verify_token = $_REQUEST['hub_verify_token'];
 if ($verify_token === 'EAAFJaTQTZCAgBABia8f4DSvVlbXRHPcZCBqp18OZCwPTsss6iQ88QIQsDTq1137AlkQ7hT023Bta8DPtiV3MA9aadKpfMjf6qzlEAKzP5XWZCOTlLFbLthxWp8JznToiLT5GqsGWYmN5Sub52mByZC7WZCeYFX4apNwbopyDS6YwZDZD') {
 echo $challenge;
 }
+?>
